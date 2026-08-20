d78183b3e9a67183b9be96528a98e4c1b1481de1
Elinor Selinder <Elinor.selinder@gmail.com>
2026-08-20 19:18:09 +0200

    shop zone and ui start

---
+++ Assets/Scripts/Interaction/S_ShoppingZone.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class S_ShoppingZone : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] GameObject interactionUI;
+    [SerializeField] GameObject shopUI;
+    [SerializeField] TextMeshProUGUI interactionText;
+    //[SerializeField] S_GameManager gameManager;
+    private InputAction _interactAction;
+    private bool inZone;
+    void Start()
+    {
+        interactionText.text = "press f to Shop";
+        _interactAction = InputSystem.actions.FindAction("Interact1");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        interactionUI.SetActive(true);
+        inZone = true;
+    }
+
+    void Update()
+    {
+        if (inZone)
+        {
+             if (_interactAction.WasPressedThisFrame())
+            {
+                shopUI.SetActive(!shopUI.activeSelf);
+            }
+        }
+    }
+    void OnTriggerStay()
+    {
+        interactionUI.SetActive(!shopUI.activeSelf);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        interactionUI.SetActive(false);
+        shopUI.SetActive(false);
+        inZone = false;
+    }
+}
--- Assets/Scripts/S_ShopManager.cs
+++ Assets/Scripts/S_ShopManager.cs
@@ -3,13 +3,8 @@ using UnityEngine;
 public class S_ShopManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] S_GameManager gameManager;
+    public void TryBuy()
     {
         
     }
