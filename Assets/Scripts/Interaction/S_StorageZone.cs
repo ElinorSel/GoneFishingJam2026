12bd0f0021b740f5bdcd7a2dce76628b978ddb21
Elinor Selinder <Elinor.selinder@gmail.com>
2026-08-20 18:41:03 +0200

    add storage zone

---
+++ Assets/Scripts/Interaction/S_StorageZone.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+public class S_StorageZone : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] GameObject interactionUI;
+    [SerializeField] GameObject storageUI;
+    [SerializeField] TextMeshProUGUI interactionText;
+    [SerializeField] S_GameManager gameManager;
+    private InputAction _interactAction;
+    private bool inZone;
+
+        void Start()
+    {
+        interactionText.text += " open storage";
+        _interactAction = InputSystem.actions.FindAction("Interact1");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        interactionUI.SetActive(true);
+        inZone = true;
+    }
+
+    void Update()
+    {
+        if (inZone)
+        {
+             if (_interactAction.WasPressedThisFrame())
+            {
+                storageUI.SetActive(!storageUI.activeSelf);
+            }
+        }
+    }
+    void OnTriggerStay()
+    {
+        interactionUI.SetActive(!storageUI.activeSelf);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        interactionUI.SetActive(false);
+        storageUI.SetActive(false);
+        inZone = false;
+    }
+}
